2959741a59ca063bddb67d29bd0d6f1ba49fc7cb
Bharath Gowda P <bharathbg882@gmail.com>
2023-07-04 21:28:58 +0530

    added analytics

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ import { Provider } from "react-redux";
 import { store } from "./redux/Store";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import {Analytics} from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+  <Analytics>
   <BrowserRouter>
     <Provider store={store}>
       <App />
       <Toaster />
     </Provider>
   </BrowserRouter>
+  </Analytics>
+
 );
